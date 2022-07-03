1dc4924adac9bba825d72024d6a9ed04c1848594
jjininu@gmail.com <jjininu@gmail.com>
2022-07-03 06:28:55 -0400

    update

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask
-from diabetic.logger import *
-from diabetic.exception import *
 
-app = Flask(__name__)
+app=Flask(__name__)
+
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    try:
-        raise Exception("We are testing custom exception")
-        return "CI CD pipeline has been established."
-    except Exception as e:
-        raise CustomException(e,sys) from e
-        
-
+    return "cicd"
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
--- setup.py
+++ setup.py
@@ -2,30 +2,24 @@ from setuptools import setup,find_packages
 from typing import List
 
 #Declaring variables for setup functions
-PROJECT_NAME="housing-predictor"
-VERSION="0.0.3"
-AUTHOR="Avnish Yadav"
+PROJECT_NAME="demo-predictor"
+VERSION="0.0.2"
+AUTHOR="Junior Eshetu"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
 REQUIREMENT_FILE_NAME="requirements.txt"
 
-HYPHEN_E_DOT = "-e ."
 
-
-def get_requirements_list() -> List[str]:
+def get_requirements_list()->List[str]:
     """
-    Description: This function is going to return list of requirement
+   
+    Description: This function is going to return list of requirement 
     mention in requirements.txt file
-    return This function is going to return a list which contain name
+    return This function is going to return a list which contain name 
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        if HYPHEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPHEN_E_DOT)
-        return requirement_list
-
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -34,4 +28,5 @@ version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
 packages=find_packages(), 
-install_requires=get_requirements_list())
+install_requires=get_requirements_list()
+)
