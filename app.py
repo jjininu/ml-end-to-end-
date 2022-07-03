30512f94847325320f3cad0b711417e42e408f6a
jjininu@gmail.com <jjininu@gmail.com>
2022-07-02 20:24:24 -0400

    update3

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 from flask import Flask
+from diabetic.logger import *
+from diabetic.exception import *
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    try:
+        raise Exception("We are testing custom exception")
+        return "CI CD pipeline has been established."
+    except Exception as e:
+        raise CustomException(e,sys) from e
+        
+
+
+if __name__=="__main__":
+    app.run()
--- diabetic/exception/__init__.py
+++ diabetic/exception/__init__.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-class HousingException(Exception):
+class CustomException(Exception):
     
     def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+        self.error_message=CustomException.get_detailed_error_message(error_message=error_message,
                                                                        error_detail=error_detail
                                                                         )
 
@@ -33,4 +33,4 @@ class HousingException(Exception):
 
 
     def __repr__(self) -> str:
-        return HousingException.__name__.str()
+        return CustomException.__name__.str()
